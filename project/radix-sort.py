eff56584e04d35897b7fdb0b921efd0fd04d58a7
Nathan Gerstein <ngerstein@hawk.iit.edu>
2021-05-16 01:39:56 -0500

    new submission for radix_sort

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -4,79 +4,30 @@ from unittest import TestCase
 import random
 
 
-################################################################################
-# EXTENSIBLE HASHTABLE
-################################################################################
-class HashTable:
-
-    def __init__(self, n_buckets=256, fillfactor=1):
-        self.n_buckets = n_buckets
-        self.fillfactor = fillfactor
-        self.buckets = [[None]] * n_buckets
-        self.nitems = 0
-
-    def __getitem__(self,  key):
-        # BEGIN_SOLUTION
-        if self.buckets[key] != None:    
-            return self.buckets[key]
-        raise KeyError
-        # END_SOLUTION
-
-    def __setitem__(self, key, value):
-        # BEGIN_SOLUTION
-        pos = key
-        self.nitems += 1
-        if self.buckets[pos] == [None]:
-            self.buckets[pos] = [(key, value)]
-        else:
-            self.buckets[pos].append((key, value))
-        # END_SOLUTION
-
-
-    def __contains__(self, key):
-        try:
-            _ = self[key]
-            return True
-        except:
-            return False
-
-    def __len__(self):
-        return self.nitems
-
-    def __bool__(self):
-        return self.__len__() != 0
-
-    def __iter__(self):
-        ### BEGIN SOLUTION
-        for i in range(self.n_buckets):
-            if self.buckets[i]:
-                for item in self.buckets[i]:
-                    if item:
-                        yield item
-        ### END SOLUTION
-
-    def items(self):
-        ### BEGIN SOLUTION
-        
-        for i in range(self.n_buckets):
-            if self.buckets[i]:
-                for item in self.buckets[i]:
-                    if item:
-                        yield item
-        ### END SOLUTION
-
-    def __str__(self):
-        return '{ ' + ', '.join(str(k) + ': ' + str(v) for k, v in self.items()) + ' }'
-
-    def __repr__(self):
-        return str(self)
-
-
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
     bookascii = booktxt.encode('ascii','replace')
     
     return bookascii.split()
+def counting_sort(idx, book = book_to_words(), length = len(book_to_words()), key_num = 256):
+    to_return = [0] * length
+    counts = [0] * (key_num + 1)
+    for item in book:
+        if len(item) > idx:
+            counts[(item[idx]) + 1] +=1
+        else:
+            counts[0] += 1
+    for i in range(len(counts)-1):
+        counts[i + 1] += counts[i]
+    for item in reversed(book):
+        if len(item) > idx:
+            index = (item[idx]) + 1
+        else:
+            index = 0
+        to_return[counts[index] - 1] = item
+        counts[index] -= 1
+    
+    return to_return
 
 def max_length(book = book_to_words()):
     max = 0
@@ -87,45 +38,12 @@ def max_length(book = book_to_words()):
     return max
 
 
-def fill_buckets(idx = 0, book = book_to_words()):
+def radix_a_book(length = len(book_to_words()), book = book_to_words(), max_length = max_length(), key_num = 256):
+   
+    for idx in range(max_length - 1, -1, -1):
+        book = counting_sort(idx, book = book, length = length, key_num = key_num)
+    return book
     
-    data = HashTable()
-    for word in book:
-        if len(word) < idx + 1:
-            data[0] = word
-        else:
-            data[word[-1 -idx]] = word
-    return data
-
-
-def LSD_sort(idx, data):
-    
-    newdata = HashTable()
-    
-    for items in data:
-        
-        if len(items[1]) < idx + 1:
-            newdata[0] = items[1]
-        else:
-            byte = items[1]
-            newdata[byte[-1 -idx]] = items[1]
-    return newdata
-
-def radix_a_book(book = book_to_words(), key = None, idx = None):
-    
-    max = max_length(book)
-    newbuckets = fill_buckets(book = book)
-    for i in range(max - 1):
-        newbuckets = LSD_sort(i + 1, newbuckets)
-    if (key == None) and (idx == None):
-        return newbuckets
-    elif (key != None) and (idx != None):
-        return newbuckets[key][idx]
-    elif (key != None) and (idx == None) :
-        return newbuckets[key]
-    
-
-
 ################################################################################d
 # TEST CASES
 ################################################################################
@@ -134,36 +52,28 @@ def radix_a_book(book = book_to_words(), key = None, idx = None):
 
 def test_simple():
     tc = TestCase()
-    test = [b'aaa' , b'aba', b'acc', b'abb', b'bbb', b'ccc', b'ccca']
-    print(f'Output is supposed to be: b\'aaa\', b\'aba\', b\'abb\', b\'acc\', b\'bbb\', b\'ccc\', b\'ccca\'')
-    print(radix_a_book(book = test))
+    test = [b'aba' , b'aaaa', b'acc', b'aaa', b'abb', b'bbb', b'ccca', b'ccc']
+    print(f'Output is supposed to be: b\'aaa\', b\'aaaa\', b\'aba\', b\'abb\', b\'acc\', b\'bbb\', b\'ccc\', b\'ccca\'')
+    print(radix_a_book(book = test, length = 8, max_length = 4))
     
-    tc.assertEqual(radix_a_book(book = test, key = 0, idx = 3), (0, b'acc'))
-
+    tc.assertEqual(radix_a_book(book = test, length = 8, max_length = 4), [b'aaa', b'aaaa', b'aba', b'abb', b'acc', b'bbb', b'ccc', b'ccca'])
 # RR-fix (simple) test
 # 10 points
 def test_simple2():
     tc = TestCase()
     test2 = [b'horse' , b'banana', b'monkey', b'bonano', b'monkiy', b'hornse', b'grasshopper', b'catfish', b'batfish']
-    print(f'Output is supposed to be: 0: b\'horse\', 0: b\'banana\', 0: b\'bonano\', 0: b\'hornse\', 0: b\'monkey\', 0: b\'monkiy\', 0: b\'batfish\', 0: b\'catfish\' 103: b\'grasshopper\' ')
-    print(radix_a_book(test2))
-    tc.assertEqual(radix_a_book(book = test2, key = 0, idx = 3), (0, b'hornse'))
-    tc.assertEqual(radix_a_book(book = test2, key = 0, idx = 1), (0, b'banana'))
-    tc.assertEqual(radix_a_book(book = test2, key = 0), [(0, b'horse'), (0, b'banana'), (0, b'bonano'), (0, b'hornse'), (0, b'monkey'), (0, b'monkiy'), (0, b'batfish'), (0, b'catfish')])
+    print(f'Output is supposed to be: [b\'banana\', b\'batfish\', b\'bonano\', b\'catfish\', b\'grasshopper\', b\'hornse\', b\'horse\', b\'monkey\', b\'monkiy\'] ')
+    print(radix_a_book(book = test2, length = 9, max_length = 11))
+    tc.assertEqual(radix_a_book(book = test2, length = 9, max_length = 11),[b'banana', b'batfish', b'bonano', b'catfish', b'grasshopper', b'hornse', b'horse', b'monkey', b'monkiy'])
+    
 # LR-fix (simple) test
 # 10 points
 def test_book1():
     tc = TestCase()
-    t = b'h'
-
-    a = radix_a_book(key = 0, idx = 1000)
-    b = radix_a_book(key = 0, idx = 3000)
-    char1 = a[1]
-    char2 = b[1]
     
-    print(f'a: {a} should come before b: {b}')
-    tc.assertEqual(radix_a_book(key = t[0], idx = 0), (104, b'https://www.gutenberg.org/8/84/'))
-    tc.assertTrue(char2[0] > char1[0])
+    
+    h = sorted(book_to_words())
+    tc.assertEqual(h, radix_a_book())
 
 
 
@@ -189,7 +99,7 @@ def main():
         t()
         say_success()
     print(80 * "#" + "\nALL TEST CASES FINISHED SUCCESSFULLY!\n" + 80 * "#")
-    print('\n Radix Sort works flawlessly!')
+    print('\n Radix Sort works flawlessly, the same as sorted()!')
 
 if __name__ == '__main__':
     main()
